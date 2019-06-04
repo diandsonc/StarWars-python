44d2b509c9627e9bb9d283d6b328ce9b2b88319b
Diandson Cardoso <diandsonc@gmail.com>
2019-06-04 00:48:27 -0300

    Mock serviço swapi

--- src/tests/test_planet_controller.py
+++ src/tests/test_planet_controller.py
@@ -1,5 +1,7 @@
 from src.tests.configure_test import app, db, TestConfig
 from src.api.models.planet import Planet
+from src.api.services.swapi.models import Planet as PlanetSwapi
+from mock.mock import call, patch
 from types import SimpleNamespace
 import json
 
@@ -19,7 +21,27 @@ def headers():
     }
 
 
-def test_list_planets_should_return_status_code_200(app):
+def get_planet(name):
+    return PlanetSwapi(
+        name=name,
+        diameter='12500',
+        climate='temperate',
+        gravity='1 standard',
+        terrain='grasslands, mountains',
+        population='2000000000',
+        created=None,
+        edited=None,
+        url=None,
+        rotation_period='24',
+        orbital_period='364',
+        surface_water='40',
+        residents=['5', '6', '8'],
+        films=['movie1', 'movie2', 'movie3']
+    )
+
+
+@patch('src.api.services.swapi.api.get_planet', return_value=get_planet('Alderaan'))
+def test_list_planets_should_return_status_code_200(mock_get_planet, app):
     app = app(TestConfig)
     client = app.test_client()
     setup_data()
@@ -27,30 +49,36 @@ def test_list_planets_should_return_status_code_200(app):
     url = '/api/v1/planets'
     response = client.get(url, headers=headers())
 
+    mock_get_planet.assert_has_calls([call('Alderaan'), call('Hoth')])
     assert response.status_code == 200
 
 
-def test_get_planet_by_name_should_return_status_code_200(app):
+@patch('src.api.services.swapi.api.get_planet', return_value=get_planet('Alderaan'))
+def test_get_planet_by_name_should_return_status_code_200(mock_get_planet, app):
     app = app(TestConfig)
     client = app.test_client()
     setup_data()
     url = '/api/v1/planets/search/name/Alderaan'
     response = client.get(url, headers=headers())
 
+    mock_get_planet.assert_called_once_with('Alderaan')
     assert response.status_code == 200
 
 
-def test_get_planet_by_id_should_return_status_code_200(app):
+@patch('src.api.services.swapi.api.get_planet', return_value=get_planet('Alderaan'))
+def test_get_planet_by_id_should_return_status_code_200(mock_get_planet, app):
     app = app(TestConfig)
     client = app.test_client()
     setup_data()
     url = '/api/v1/planets/search/id/1'
     response = client.get(url, headers=headers())
 
+    mock_get_planet.assert_called_once_with('Alderaan')
     assert response.status_code == 200
 
 
-def test_get_planet_by_id_return_json_with_filled_model(app):
+@patch('src.api.services.swapi.api.get_planet', return_value=get_planet('Alderaan'))
+def test_get_planet_by_id_return_json_with_filled_model(mock_get_planet, app):
     app = app(TestConfig)
     client = app.test_client()
     setup_data()
@@ -59,7 +87,7 @@ def test_get_planet_by_id_return_json_with_filled_model(app):
     data = json.loads(response.data, object_hook=lambda d: SimpleNamespace(**d)).data
 
     assert 1 == data.id
-    assert 2 == data.films
+    assert 3 == data.films
     assert 'Alderaan' == data.name
     assert 'temperate' == data.climate
     assert 'grasslands, mountains' == data.terrain
@@ -96,7 +124,8 @@ def test_delete_planet_by_id_should_return_status_code_404(app):
     assert response.status_code == 404
 
 
-def test_create_shoud_persist_data_and_return_status_code_200(app):
+@patch('src.api.services.swapi.api.get_planet', return_value=get_planet('Alderaan'))
+def test_create_shoud_persist_data_and_return_status_code_200(mock_get_planet, app):
     app = app(TestConfig)
     client = app.test_client()
     planet = {
@@ -107,6 +136,7 @@ def test_create_shoud_persist_data_and_return_status_code_200(app):
     url = '/api/v1/planets'
     response = client.post(url, data=json.dumps(planet), headers=headers())
 
+    mock_get_planet.assert_called_once_with('Alderaan')
     assert response.status_code == 200
 
 
@@ -152,7 +182,8 @@ def test_create_when_terrain_is_null_shoud_return_status_code_400_bad_request(ap
     assert response.status_code == 400
 
 
-def test_update_should_persist_data_and_return_status_code_200(app):
+@patch('src.api.services.swapi.api.get_planet', return_value=get_planet('Alderaan'))
+def test_update_should_persist_data_and_return_status_code_200(mock_get_planet, app):
     app = app(TestConfig)
     client = app.test_client()
     setup_data()
@@ -164,6 +195,7 @@ def test_update_should_persist_data_and_return_status_code_200(app):
     url = '/api/v1/planets/2'
     response = client.put(url, data=json.dumps(planet), headers=headers())
 
+    mock_get_planet.assert_called_once_with('Alderaan')
     assert response.status_code == 200
 
 
